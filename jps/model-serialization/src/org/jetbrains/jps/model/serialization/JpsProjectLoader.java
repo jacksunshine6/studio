1c39b871928eb4407e021686266c099016db4289
Michal Bendowski <bendowski@google.com>
2014-11-21 17:14:08 +0000

    Apply Android Studio branding to tip of the day.
    And a few other places.
    
    Change-Id: Id50e4fefd9a30ad6dd11690b4ec28ca0fe3d7a82
    
    Conflicts:
    	platform/lang-impl/src/com/intellij/openapi/vcs/checkin/CodeAnalysisBeforeCheckinHandler.java
    	resources-en/src/tips/FinishBySmartEnter.html

--- jps/model-serialization/src/org/jetbrains/jps/model/serialization/JpsProjectLoader.java
+++ jps/model-serialization/src/org/jetbrains/jps/model/serialization/JpsProjectLoader.java
@@ -95,7 +95,7 @@ public class JpsProjectLoader extends JpsLoaderBase {
         directory = file;
       }
       else {
-        throw new IOException("Cannot find IntelliJ IDEA project files at " + projectPath);
+        throw new IOException("Cannot find Android Studio project files at " + projectPath);
       }
       new JpsProjectLoader(project, pathVariables, directory.getParentFile()).loadFromDirectory(directory);
     }
