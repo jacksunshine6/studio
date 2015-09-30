7c7271ecec8320479d8f614a20c055d712b1dbc7
Deepanshu Gupta <deepanshu@google.com>
2015-09-29 17:23:07 -0700

    Set defaults for build_studio.sh
    - Search for java 6 and java 8 at a few locations if appropriate env
      vars are not set.
    - The env vars for java are JAVA_HOME and JAVA8_HOME.
    - If no args are present, use the default from the ant build.
    
    Change-Id: Iaae3b5b15170b0f824a0c8a3442a51e092701bdb

--- build_studio.sh
+++ build_studio.sh
@@ -11,46 +11,93 @@ PROG_DIR=$(dirname "$0")
 
 function die() {
   echo "$*" > /dev/stderr
-  echo "Usage: $0 <out_dir> <dest_dir> <build_number> [--enable-uitests]" > /dev/stderr
+  echo "Usage: $0 [<out_dir> <dist_dir> <build_number>] [--enable-uitests]" > /dev/stderr
   exit 1
+}
+
+function set_java_home() {
+  if [ ! "$JAVA_HOME" ]; then
+    case `uname -s` in
+        Darwin)
+            export JAVA_HOME=$(/usr/libexec/java_home -v 1.6)
+            ;;
+        *)
+            if [[ -s /usr/lib/jvm/java-6-sun ]]; then
+              export JAVA_HOME=/usr/lib/jvm/java-6-sun
+            else
+              die "java 1.6 not found. set JAVA_HOME."
+            fi
+            ;;
+    esac
+  fi
+}
+
+function set_java8_home() {
+  if [ ! "$JAVA8_HOME" ]; then
+    case `uname -s` in
+        Darwin)
+            export JAVA8_HOME=$(/usr/libexec/java_home -v 1.8)
+            ;;
+        *)
+            jdk8=`find /usr/lib/jvm/ -maxdepth 1 -name jdk1.8* | sort -V -r | head -n1`
+            if [[ -s "$jdk8" ]]; then
+              export JAVA8_HOME="$jdk8"
+            else
+              die "java 1.8 not found. set JAVA8_HOME."
+            fi
+            ;;
+    esac
+  fi
 }
 
 UI_TESTS=
 while [[ -n "$1" ]]; do
-  if [[ -z "$OUT" ]]; then
+  if [[ $1 == "--enable-uitests" ]]; then
+    UI_TESTS=1
+  elif [[ -z "$OUT" ]]; then
     OUT="$1"
   elif [[ -z "$DIST" ]]; then
     DIST="$1"
   elif [[ -z "$BNUM" ]]; then
     BNUM="$1"
-  elif [[ $1 == "--enable-uitests" ]]; then
-    UI_TESTS=1
   else
     die "[$0] Unknown parameter: $1"
   fi
   shift
 done
 
-if [[ -z "$OUT" ]]; then die "## Error: Missing out folder"; fi
-if [[ -z "$DIST" ]]; then die "## Error: Missing destination folder"; fi
-if [[ -z "$BNUM" ]]; then die "## Error: Missing build number"; fi
+#if $OUT is not set, then none of the values are set.
+if [[ -z "$OUT" ]]; then
+  OUT="$PROG_DIR"/out
+  DIST="$OUT"/dist
+  BNUM=SNAPSHOT
+else
+  if [[ -z "$DIST" ]]; then die "## Error: Missing distribution folder"; fi
+  if [[ -z "$BNUM" ]]; then die "## Error: Missing build number"; fi
+fi
 
 cd "$PROG_DIR"
-
 mkdir -p "$OUT"
+mkdir -p "$DIST"
 
 ANT="java -jar lib/ant/lib/ant-launcher.jar -f build.xml"
 
 echo "## Building android-studio ##"
-echo "## Dest dir : $DIST"
+echo "## Dist dir : $DIST"
 echo "## Qualifier: $QUAL"
 echo "## Build Num: $BNUM"
 echo "## UI Tests : $UI_TESTS"
 echo
 
+set_java_home
+set_java8_home
+
+export JDK_16_x64=$JAVA_HOME
+export JDK_18_x64=$JAVA8_HOME
+
 $ANT "-Dout=$OUT" "-Dbuild=$BNUM" "-Denable.ui.tests=$UI_TESTS"
 
-echo "## Copying android-studio destination files"
+echo "## Copying android-studio distribution files"
 cp -rfv "$OUT"/artifacts/android-studio* "$DIST"/
 cp -rfv "$OUT"/updater-full.jar "$DIST"/android-studio-updater.jar
 (cd ../adt/idea/native/installer/win && zip -r - ".") > "$DIST"/android-studio-bundle-data.zip
