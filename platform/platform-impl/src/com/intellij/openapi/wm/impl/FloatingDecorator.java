232daed63728d28f30917aee42951b52a188f784
Jens Ole Lauridsen <jlauridsen@google.com>
2015-10-23 17:10:49 -0700

    Change the FloatingDecorator to use a JFrame instead of a JDialog.
    The ToolWindows in Intellij is using JDialog which has problems on
    MacOS. All the windows are not modal so we might as well use a JFrame
    instead.
    
    Bug: http://b.android.com/77779
    
    Change-Id: I41e2f953ababf61ae0bfc7108f0fedb2e64efbfa

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/FloatingDecorator.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/FloatingDecorator.java
@@ -40,7 +40,7 @@ import java.awt.event.WindowEvent;
  * @author Anton Katilin
  * @author Vladimir Kondratyev
  */
-public final class FloatingDecorator extends JDialog {
+public final class FloatingDecorator extends JFrame {
   private static final Logger LOG=Logger.getInstance("#com.intellij.openapi.wm.impl.FloatingDecorator");
 
   static final int DIVIDER_WIDTH = 3;
@@ -66,8 +66,8 @@ public final class FloatingDecorator extends JDialog {
   private float myEndRatio; // start and end alpha ratio for transparency animation
 
 
-  FloatingDecorator(final IdeFrameImpl owner,final WindowInfoImpl info,final InternalDecorator internalDecorator){
-    super(owner,internalDecorator.getToolWindow().getId());
+  FloatingDecorator(final IdeFrameImpl unused,final WindowInfoImpl info,final InternalDecorator internalDecorator){
+    super(internalDecorator.getToolWindow().getId());
     MnemonicHelper.init(getContentPane());
     myInternalDecorator=internalDecorator;
 
